1ebfadf5585dc89314a94862b6033e5dfedf8df9
Hari Setiaji <hari.setiaji2000@gmail.com>
2023-02-18 20:44:44 +0700

    Initial commit

---
+++ src/test/java/com/setianjay/springboot/learnspringboot/LearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.setianjay.springboot.learnspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
